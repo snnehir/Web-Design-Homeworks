d2fb4899c784b015c7ee6f414959352bc12c5e09
mint <snnehir21@gmail.com>
2022-05-12 15:21:25 +0300

    Activity8: Using jQuery UI (update)

--- activity8/Activity8.js
+++ activity8/Activity8.js
@@ -1,8 +1,8 @@
-window.onload = function(){
+$(document).ready(function(){
     // Part 3: date picker
     $("#birthdate").datepicker();
 
     // Part 4: auto completion
     const programming_languages = ["ActionScript", "AppleScript", "Asp", "JavaScript", "Lisp", "Perl", "PHP", "Python"]
     $("#pl").autocomplete({source: programming_languages})
-}
+})
