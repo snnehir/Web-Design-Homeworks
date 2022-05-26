b8f64a700d87892e3a55292d2b93d81c6eaeb4b1
mint <snnehir21@gmail.com>
2022-05-26 09:33:18 +0300

    Activity 11 - Slider

---
+++ assignment3/Activity11_Slider/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10,
+		autoDelay: 3000,
+		randomStart: true,
+		captions: true,
+		pagerType: "short"
+
+	});
+});
+		
